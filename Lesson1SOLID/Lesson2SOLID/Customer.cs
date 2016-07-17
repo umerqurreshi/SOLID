96f7d98547a8edfe3335413918b78886d88e628d
Umer QurreshI <ume136@hotmail.com>
2016-07-17 20:14:55 +0100

    Lesson 2 added SOLID O

---
+++ Lesson1SOLID/Lesson2SOLID/Customer.cs
@@ -0,0 +1,29 @@
+﻿namespace Lesson2SOLID
+{
+
+    // Violation of 'O' in SOLID. Class should be open for extensions but closed for
+    // MODIFICATIONS...now say if we have additional CustType....we would need to add another
+    // if condition and therefore the customer class will have been MODIFIED!!
+    class Customer
+    {
+        private int _CustType;
+
+        public int CustType
+        {
+            get { return _CustType; }
+            set { _CustType = value; }
+        }
+
+        public double getDiscount(double TotalSales)
+        {
+            if (_CustType == 1)
+            {
+                return TotalSales - 100;
+            }
+            else
+            {
+                return TotalSales - 50;
+            }
+        }
+    }
+}
